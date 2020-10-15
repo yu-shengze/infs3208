40e23b6df7b317094929a14d53380e42bdb78355
luke yu <shengze.yu@uq.net.au>
2020-10-15 10:06:18 +1000

    typo

--- sitepages/dbconnect.php
+++ sitepages/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('127.0.0.1', 'root', 'MyDBRoot123');
+$connection = mysqli_connect('127.0.0.1', 'web', '0dWK9fp%wgFN');
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
 }
